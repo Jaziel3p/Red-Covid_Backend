817608d593ea4e4766f0642ebcfc67a2d15b96a0
jaziel <jazielvega22@gmail.com>
2023-05-20 14:34:56 -0600

    Protegiendo con permisos las vistas con DjangoModelPermission

--- principal/views.py
+++ principal/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework import status, viewsets
@@ -18,21 +18,37 @@ from .models import Paciente, Diagnostico, ImagenRad
 from .serializers import PacienteSerializer, DiagnosticoSerializer, ImagenRadSerializer
 
 
+
+class LimitedGroupGetModelPermissions(DjangoModelPermissions): #Clase personalizada que hereda de DMP para modificar su acceso por default al metodo GET
+    def has_permission(self, request, view):                   #para aplicarlo al grupo de usuarios "Trabajador(a)_Social"
+        if request.method == 'GET':
+            user = request.user
+            # Verificar si el usuario tiene permiso
+            if user.is_authenticated and user.groups.filter(name__in=['Trabajador(a)_Social']).exists():
+                return False
+        return super().has_permission(request, view)
+
+
+
 class PacienteList(generics.ListCreateAPIView):
     queryset = Paciente.objects.all()
     serializer_class = PacienteSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, DjangoModelPermissions)
     authentication_class = (TokenAuthentication,)
 
 
 class DiagnosticoView(viewsets.ModelViewSet):
     queryset = Diagnostico.objects.all()
     serializer_class = DiagnosticoSerializer
+    permission_classes = (IsAuthenticated, LimitedGroupGetModelPermissions)
+    authentication_class = (TokenAuthentication,)
 
 
 class ImagenRadView(viewsets.ModelViewSet):
     queryset = ImagenRad.objects.all()
     serializer_class = ImagenRadSerializer
+    permission_classes = (IsAuthenticated, DjangoModelPermissions)
+    authentication_class = (TokenAuthentication,)
 
 
 class Login(FormView):
